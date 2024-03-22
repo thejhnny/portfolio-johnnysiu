eb1114676579b19a57ef47ed4422f04e21d05657
Johnny Siu <johnnysiu@Johnnys-MBP.attlocal.net>
2024-03-22 15:55:28 -0700

    removing console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ type Props = {
 };
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
-  console.log(socials);
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#556B2F]">
       <Head>
